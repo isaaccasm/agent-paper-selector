22ffe6b2f8c07378b0082c932c67bdd185a4a77a
Isaac Castro Mateos <isaac.casm@gmail.com>
2025-03-15 00:02:18 +0000

    Utils to download and read files

---
+++ agent_paper_selector/arxiv_utils.py
@@ -0,0 +1,51 @@
+import os
+import tempfile
+from typing import Dict, List
+import arxiv
+import pymupdf4llm
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+def fetch_papers(topic: str, max_results: int = 10) -> List[Dict]:
+    """
+    Fetch a number of papers from arxiv given a topic and a maximum number of them
+    :param topic:
+    :param max_results:
+    :return:
+    """
+    client = arxiv.Client()
+    search = arxiv.Search(query=topic,
+                           max_results=max_results,
+                           sort_by=arxiv.SortCriterion.SubmittedDate)
+    papers = []
+    for result in client.results(search):
+        papers.append({
+            "title": result.title,
+            "summary": result.summary,
+            "published": result.published.strftime("%Y-%m-%d"),
+            "id": result.entry_id
+        })
+    return papers
+
+
+def split_text(text, chunk_size=1000, chunk_overlap=100):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    return splitter.split_text(text)
+
+
+def add_content_to_papers(papers: List[Dict]) -> List[Dict]:
+    """
+    Add the content of the paper to each of the dictionaries. It is assume that each paper
+    has the key 'id'.
+    :param papers: A list o dictionaries with at least the key 'id'
+    :return:
+    """
+    temporary_file = 'temporary_file.pdf'
+    for paper in papers:
+        paper_obj = next(arxiv.Client().results(arxiv.Search(id_list=[paper['id']])))
+        paper_obj.download_pdf(filename=temporary_file)
+        content = pymupdf4llm.to_markdown(temporary_file)
+        paper['content'] = content
+
+    os.remove(temporary_file)
+    return papers
